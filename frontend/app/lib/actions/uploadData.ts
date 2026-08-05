053caf836a4cc2f044b6901d75ba49b12acd76e5
Aaditya Singh <aadityasingh9601@gmail.com>
2026-08-05 15:30:19 +0000

    url fix in uploadData.ts

--- frontend/app/lib/actions/uploadData.ts
+++ frontend/app/lib/actions/uploadData.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 export const uploadData = async (userId: any, data: any) => {
   const formData = new FormData();
   formData.append("file", data);
-  const res = await axios.post(`/api/upload?userId=${userId}`, formData, {});
+  const res = await axios.post(`${process.env.BACKEND_URL}/api/upload?userId=${userId}`, formData, {});
   return {
     success: true,
     message: res.data,
